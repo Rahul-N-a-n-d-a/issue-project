bbd99098d3c2cf0eb26cf40ff6af3ff2993bd9f5
Rahul Nanda <rahulnandakca@gmail.com>
2023-11-01 18:00:51 +0530

    done

---
+++ Backend/Schema/userSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        name: String,
+        password: String
+    },
+    { timestamps: true }
+);
+const User = mongoose.model('userDetails', userSchema);
+module.exports = User;
+
---
+++ Backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const CORS = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const dbURL = "mongodb://localhost:27017/userData";
+
+mongoose
+  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) =>
+    app.listen(PORT, () => {
+      console.log("Started at ==>", PORT);
+    })
+  )
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(CORS());
